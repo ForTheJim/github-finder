a5ed5f1d3e781219d9babdc0eb6c0f824b0096a6
Jim Olguin <jim@forthejim.com>
2020-10-06 21:55:22 -0700

    - update favicon

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,8 @@
 const tailwindcss = require("tailwindcss");
 module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
   plugins: [tailwindcss("./tailwind.js"), require("autoprefixer")],
 };
